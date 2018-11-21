d2a760bd2d0869344d0f1e2a6bb84ed5346bcabe
stiefel40k <stiefel40k@hotmail.com>
2018-11-21 17:27:00 +0000

    add libminshacrypt target as it is needed, and another component cries if it is not there

--- libmincrypt/Android.mk
+++ libmincrypt/Android.mk
@@ -13,6 +13,12 @@ LOCAL_MODULE := libmincrypt
 LOCAL_SRC_FILES := rsa.c sha.c sha256.c
 include $(BUILD_HOST_STATIC_LIBRARY)
 
+## Crippled version without an RSA implementation
+## to coexist with libcrypto_static and provide SHA_hash
+include $(CLEAR_VARS)
+LOCAL_MODULE := libminshacrypt
+LOCAL_SRC_FILES := sha.c sha256.c
+include $(BUILD_STATIC_LIBRARY)
 
 include $(LOCAL_PATH)/tools/Android.mk \
         $(LOCAL_PATH)/test/Android.mk
